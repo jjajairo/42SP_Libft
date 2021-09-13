943b631b3f19deb400ac32d819a03e95d9a03e4e
Jairo Caetano <42660842+jjajairo@users.noreply.github.com>
2021-09-13 13:47:43 -0300

    Fixed memmove teste

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,25 +6,42 @@
 /*   By: jcaetano <jcaetano@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 08:06:35 by jcaetano          #+#    #+#             */
-/*   Updated: 2021/09/13 13:44:17 by jcaetano         ###   ########.fr       */
+/*   Updated: 2021/09/13 13:47:33 by jcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t size)
+// void	*ft_memmove(void *dest, const void *src, size_t size)
+// {
+// 	char	*s;
+// 	char	*d;
+
+// 	s = (char *)src;
+// 	d = (char *)dest;
+// 	if (s > d)
+// 	{
+// 		ft_memcpy(d, s, size);
+// 		return (dest);
+// 	}
+// 	while (size--)
+// 		d[size] = s[size];
+// 	return (dest);
+// }
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char	*s;
 	char	*d;
 
-	s = (char *)src;
-	d = (char *)dest;
-	if (s > d)
+	s = (char *) src;
+	d = (char *) dest;
+	if (s < d)
 	{
-		ft_memcpy(d, s, size);
+		while (n--)
+			d[n] = s[n];
 		return (dest);
 	}
-	while (size--)
-		d[size] = s[size];
+	ft_memcpy(d, s, n);
 	return (dest);
 }
